5726a34043243998c786edd52c540f8c6c6b89d8
apurva <apurvaraut9@gmail.com>
2019-03-20 16:26:02 +0530

    react ui done

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 
+import { Provider} from 'react-redux';
+import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import ShoppingList from './components/ShoppingList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <AppNavbar />
+        <ShoppingList />
       </div>
+
+      </Provider>
     );
   }
 }
